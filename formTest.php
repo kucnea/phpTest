f623fa91e635a3f5c1d6f0946a372d9a735af92c
kucnea@naver.com <kucnea@naver.com>
2022-02-18 16:40:59 +0900

    from Post 테스트

--- formTest.php
+++ formTest.php
@@ -1,1 +1,20 @@
 <?php
+
+
+
+
+?>
+
+<html>
+<body>
+
+	<form action="request.php" method="post">
+	
+		name : <input type="text" name="name">
+		email : <input type="text" name="email">
+		<input type="submit">
+		
+	</form>
+
+</body>
+</html>
